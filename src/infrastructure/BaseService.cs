5746fead8b0c539c3f2765f16e592ff82db16708
qomsad <qomsad@users.noreply.github.com>
2023-12-15 19:57:13 +0500

    Base Service

---
+++ src/infrastructure/BaseService.cs
@@ -0,0 +1,19 @@
+using AnimalsService.Config;
+using Sieve.Models;
+using Sieve.Services;
+
+namespace AnimalsService.Infrastructure
+{
+  public abstract class BaseService<T>(ApplicationContext _context, ISieveProcessor _sieve)
+  {
+    protected readonly ApplicationContext context = _context;
+    protected readonly ISieveProcessor sieve = _sieve;
+
+    public abstract T Create(T entity);
+    public abstract void Delete(long id);
+    public abstract bool Exists(long id);
+    public abstract IEnumerable<T> GetList(SieveModel param);
+    public abstract T? GetOne(long id);
+    public abstract T Update(long id, T entity);
+  }
+}
